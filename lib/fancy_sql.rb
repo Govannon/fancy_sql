9763564f00f575518da72c676a19d691ee57ce1c
Wes Oldenbeuving <narnach@gmail.com>
2010-07-21 13:40:20 +0200

    Let the Mysql gem build the row hash, but symbolize it for ease of use.

--- lib/fancy_sql.rb
+++ lib/fancy_sql.rb
@@ -1,29 +1,18 @@
 class ActiveRecord::Base
   # Execute sql on the ActiveRecord connection, but do a couple of extra's:
-  # * Convert each result row into a hash with the given +attributes+ as keys. Array indices are so C.
-  # * Free the result set after usage. Did not know you had to free results from ActiveRecord::Base.connnection.execute? Now you don't have to remember it, either.
-  # Note: the order of attributes must match the order of the columns resulting from the SQL. Pass a nil attribute to ignore that column.
+  # * Convert each result row into a hash with symbolized keys. Array indices are so C.
+  # * Free the result set after usage. Did not know you had to free results from ActiveRecord::Base.connnection.execute? Me neither, but now the both of us can forget about it.
   #
   # Basic example:
-  #   Post.fancy_sql("SELECT name, id FROM posts WHERE id < 3", :name, :created_at)
+  #   Post.fancy_sql("SELECT name, id FROM posts WHERE id < 3")
   #   # => [{:name=>"First post", :id=>"1"}, {:name=>"Second post", :id=>"2"}]
-  #
-  # Example that ignores the +id+ column:
-  #   Post.fancy_sql("SELECT id, name FROM posts WHERE id < 3", nil, :created_at)
-  #   # => [{:name=>"First post"}, {:name=>"Second post"}]
-  def self.fancy_sql(sql, *attributes)
-    attributes = attributes.flatten
-    rows = self.connection.execute sql
+  def self.fancy_sql(sql)
+    query_results = self.connection.execute sql
     results = Array.new
-    rows.each do |row|
-      fancy_row = Hash.new
-      attributes.each_with_index do |attribute, index|
-        next if attribute.nil?
-        fancy_row[attribute] = row[index]
-      end
-      results << fancy_row
+    while row = query_results.fetch_hash
+      results << row.symbolize_keys
     end
-    rows.free
+    query_results.free
     results
   end
 end
--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@ After writing a couple of loops that process the results of ActiveRecord::Base.c
 This plugin does the following for you:
 
 * Execute your SQL on the class's database connection.
-* Convert each result row into a hash. Doing data lookups with Array indices is so C.
+* Convert each result row into a hash with symbolized keys. Doing data lookups with Array indices is so C.
 * Free the result set after usage. I did not know this either, but now I won't have to remember it, either.
 
 Example
@@ -14,12 +14,7 @@ Example
 
 Basic example:
 
-    Post.fancy_sql("SELECT name, id FROM posts WHERE id < 3", :name, :created_at)
+    Post.fancy_sql("SELECT name, id FROM posts WHERE id < 3")
     # => [{:name=>"First post", :id=>"1"}, {:name=>"Second post", :id=>"2"}]
-
-Example that ignores the +id+ column:
-
-    Post.fancy_sql("SELECT id, name FROM posts WHERE id < 3", nil, :created_at)
-    # => [{:name=>"First post"}, {:name=>"Second post"}]
 
 Copyright (c) 2010 Wes Oldenbeuving, released under the MIT license
